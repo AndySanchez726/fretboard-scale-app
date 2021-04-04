22743e1a0e1f2f5890561b5117efbd86c1ebe3b6
Andy Sanchez <andysanchez726@gmail.com>
2021-04-04 01:57:59 -0500

    scale is now held in an object to show name and chroma. This way the name of the note is displayed instead of the chroma number. Other numbers don't matter since when the design is done it'll just be empty

---
+++ fretboard-scale-app/src/assets/tunings.js
@@ -0,0 +1,46 @@
+const tunings = [
+        {
+            string: 6,
+            tuning: ['E', 'A', 'D', 'G', 'B', 'E'],
+            name: 'Standard',
+            instrument: 'Guitar'
+        },
+        {
+            string: 6,
+            tuning: ['D', 'A', 'D', 'G', 'B', 'E'],
+            name: 'Drop D',
+            instrument: 'Guitar'
+        },
+        {
+            string: 6,
+            tuning: ['C', 'G', 'C', 'F', 'A', 'D'],
+            name: 'Drop C',
+            instrument: 'Guitar'
+        },
+        {
+            string: 6,
+            tuning: ['B', 'Gb', 'B', 'E', 'Ab', 'Db'],
+            name: 'Drop B',
+            instrument: 'Guitar'
+        },
+        {
+            string: 6,
+            tuning: ['A', 'E', 'A', 'D', 'F#', 'B'],
+            name: 'Drop A',
+            instrument: 'Guitar'
+        },
+        {
+            string: 7,
+            tuning: ['A', 'E', 'A', 'D', 'G', 'B', 'E'],
+            name: 'Drop A',
+            instrument: 'Guitar'
+        },
+        {
+            string: 4,
+            tuning: ['E', 'A', 'D', 'G'],
+            name: 'Standard',
+            instrument: 'Bass'
+        }
+    ]
+
+export { tunings };
--- fretboard-scale-app/src/components/Selector/index.js
+++ fretboard-scale-app/src/components/Selector/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Scale, Note, ScaleType } from '@tonaljs/tonal'
+import { Scale, Note, ScaleType } from '@tonaljs/tonal';
+import { tunings } from '../../assets/tunings.js'
 
 
 function Selector() {
@@ -9,22 +10,8 @@ function Selector() {
     const {noteDropdown, scaleDropdown} = formState
     var fretboardChroma = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
     var newArray = []
+    const scaleArray = []
     var numberOfStrings = ['4', '5', '6', '7', '8', '9']
-    var tunings = [
-        {
-            string: 6,
-            tuning: ['E', 'A', 'D', 'G', 'B', 'E'],
-            name: 'Standard',
-            instrument: 'Guitar'
-        },
-        {
-            string: 6,
-            tuning: ['B', 'Gb', 'B', 'E', 'Ab', 'Db'],
-            name: 'Drop B',
-            instrument: 'Guitar'
-        }
-    ]
-    console.log(tunings[0].name)
 
     function handleChange(e) {
         setFormState({...formState, [e.target.name]: e.target.value})
@@ -32,11 +19,16 @@ function Selector() {
         
     function handleSubmit(e) {
         e.preventDefault()
+        var fretboardDiv = document.getElementById('fretboard')
+        // clears fretboard
+        while(fretboardDiv.firstChild) {
+            fretboardDiv.removeChild(fretboardDiv.firstChild)
+        }
         var stringNote = Note.chroma(formState.stringNoteDropdown)
         var fullScale = Scale.get(formState.noteDropdown + ' ' + formState.scaleDropdown)
-        var tuningArray = formState.tuningDropdown.split('')
+        console.log(fullScale)
+        var tuningArray = formState.tuningDropdown.split(/(?=[A-Z])/)
         var tuningChroma = []
-        console.log(tuningArray)
         // turns selected tuning into chroma
         for ( var i = 0; i < tuningArray.length; i++) {
             tuningChroma.push(Note.chroma(tuningArray[i]))
@@ -53,37 +45,46 @@ function Selector() {
         fretboard.appendChild(node)
         document.getElementById('fretboard').appendChild(fretboard)
         var fullScaleChroma = []
+        // creates object for the scale
         for (var i = 0; i < fullScale.notes.length; i++) {
             fullScaleChroma.push(Note.chroma(fullScale.notes[i]))
+            const scaleObject = new Object();
+            scaleObject.chroma = Note.chroma(fullScale.notes[i])
+            scaleObject.name = fullScale.notes[i]
+            scaleArray.push(scaleObject)
         }
         var noteChroma = Note.chroma(formState.noteDropdown)
         offsetFunction(tuningChroma, fretboardChroma)
-        matchArrays(newArray, fullScaleChroma, formState.numberOfStringDropdown);
+        matchArrays(newArray, fullScaleChroma, formState.numberOfStringDropdown, fullScale);
         
     }
 
     // hides and shows notes in the scale
-    function matchArrays(base, toSearch, stringNumber) {
+    function matchArrays(base, toSearch, stringNumber, fullScale) {
         // reverses the orientation of the strings 
         var reverseBase = base.reverse();
+        console.log(fullScale.notes)
+        console.log(toSearch)
         var fretboardDiv = document.getElementById('fretboard')
-        // loop through strings offset arrays
+        // loop through strings 
         for ( var j = 0; j < base.length; j++) {
             var  string = 'string' + j
-            console.log(string)
             string = document.createElement('div')
             string.className = 'String'
             fretboardDiv.appendChild(string)
             var newStringArray = (base[j].[0])
-            console.log(newStringArray)
-            // loop through individual string offset arrays to get notes in key
+            // loop through notes on string match to scale chroma
             for (var i = 0; i < newStringArray.length ; i++) {   
-                var matchedFrets = toSearch.includes(newStringArray[i])
-                console.log(toSearch)
+                for (var p = 0; p < scaleArray.length; p++) {
+                    var matchedFrets = scaleArray[p].chroma === newStringArray[i]
+                    if(matchedFrets) {
+                        break;
+                    }
+                }
                 if(matchedFrets) {
                     var visibleFretEl = document.createElement('span')
                     visibleFretEl.className = 'visible'
-                    var fretNode = document.createTextNode(newStringArray[i])
+                    var fretNode = document.createTextNode(scaleArray[p].name)
                     visibleFretEl.appendChild(fretNode)
                     string.appendChild(visibleFretEl)
                 } else {
@@ -93,6 +94,7 @@ function Selector() {
                     hiddenFretEl.appendChild(fretNode)
                     string.appendChild(hiddenFretEl)
                 }
+                
             }
         }
     }
@@ -118,20 +120,20 @@ function Selector() {
         <div id='main'>
             <h1>Choose a Scale</h1>
             <form onSubmit={handleSubmit}>
-                {/* number of strings */}
+                {/* number of strings
                 <select name='numberOfStringDropdown' id='numberOfStringDropdown' onChange={handleChange}>
                     <option disabled selected value> -# of Strings- </option>
                     {numberOfStrings.map(string => (
                         <option name='string' value={string}>{string}</option>
                     ))}
-                </select>
-                {/* string note */}
+                </select> */}
+                {/* string note
                 <select name='stringNoteDropdown' id='stringNoteDropdown' onChange={handleChange}>
                     <option disabled selected value> -String Note- </option>
                     {allNotes.map(stringNote => (
                         <option name='stringNote' value={stringNote}>{stringNote}</option>
                     ))}
-                </select>
+                </select> */}
                 {/* root note dropdown */}
                 <select name="noteDropdown" id="noteDropdown" onChange={handleChange}>
                     <option disabled selected value> -Note- </option>
